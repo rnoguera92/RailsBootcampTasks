11e691bbe01bce86d2fa739ddc527ec7014d6222
Roberto Noguera <36283354+rnoguera92@users.noreply.github.com>
2021-11-29 18:23:05 -0600

    feat: added Bootstrap
    -added bootstrap
    -added sidebar and header

--- activity_02/config/routes.rb
+++ activity_02/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :games
   resources :platforms
   resources :companies
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
